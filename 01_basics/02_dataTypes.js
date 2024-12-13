fa50024d8d5f3efa404befac8ca18def3bcf80da
Aryan Parashar <aryanparashar649@gmail.com>
2024-12-13 07:09:32 +0000

    Datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/*  alert("3+3") we are using nodeJS not browser
+alert(3+3)  */
+
+
+let name = "Aryan"
+let age = "20"
+let isLOggedIn = "false"
+
+/* DataTypes 
+number => 2 to power 53
+bigInt
+string => ""
+boolean => true/false
+symbol => unique
+null => standalone value
+undefined => 
+    
+   ( difference b/w null and undefined is "let i want temperature from server 
+and server is unable to send temp. then it will saide to be undefined because 0 is also a teme.") */
+
+
+console.log(typeof "aryan");
+// this is string
+console.log(typeof undefined);
+// it is itself a type
+console.log(typeof null);
+// it is a object 
