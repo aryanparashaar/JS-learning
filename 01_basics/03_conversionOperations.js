9783b881d797d9763f244332aa6a7bc635586fa2
Aryan Parashar <aryanparashar649@gmail.com>
2024-12-13 16:19:01 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,20 @@
+let score = "23"
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+/* numbers and conversion
+"33"=> 33
+"33abc" => NaN
+true => 1 ; false => 0 */
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+/*
+1=> true ; 0=>false
+"" => false ; "anyValue" => true
+
