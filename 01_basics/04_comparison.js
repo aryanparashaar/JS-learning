a5dd568437f1ec0f9585ced891f5aa423450f50b
Aryan Parashar <aryanparashar649@gmail.com>
2024-12-13 17:26:31 +0000

    comparison in datatypes

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,10 @@
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+
+// === strict check
+console.log("2"===2);   //this will be false because === will check data type also
+
+
+
