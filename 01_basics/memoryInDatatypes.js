c30f8c13dd94a223721adcc7bbab4f8a167f224f
Aryan Parashar <aryanparashar649@gmail.com>
2024-12-13 17:50:32 +0000

    types of memory in datatypes

---
+++ 01_basics/memoryInDatatypes.js
@@ -0,0 +1,22 @@
+// stack (premetive)       Heap(non-premetive)
+let myGitname = "aryanparashaar"
+let anothername = myGitname
+anothername = "aryan_parashar"
+
+console.log(myGitname);
+console.log(anothername);
+
+let userOne = {
+    email: "user@google.com"
+    
+      
+}
+let userTwo = userOne
+
+userTwo.email = "aryan@google.com"
+
+console.log(userOne);
+console.log(userTwo);
+
+
+
